91ddfcfcfb6481ef942756ee60def68ca9eb453d
João Grando <jjdevv05@gmail.com>
2026-02-16 21:59:35 -0300

    fix: ajuste de migration flyway e repository de Services

--- src/main/java/com/nailart/infrastructure/persistence/repository/ServiceJpaRepository.java
+++ src/main/java/com/nailart/infrastructure/persistence/repository/ServiceJpaRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ServiceJpaRepository extends JpaRepository<ServiceEntity, UUID> {
 
-    @Query("SELECT DISTINCT s FROM ServiceEntity s LEFT JOIN FETCH s.options WHERE s.active = true")
+    @Query("SELECT s FROM ServiceEntity s LEFT JOIN FETCH s.options WHERE s.active = true")
     List<ServiceEntity> findByActiveTrue();
+
+    @Query("SELECT s FROM ServiceEntity s LEFT JOIN FETCH s.options WHERE s.id = :id")
+    Optional<ServiceEntity> findByIdWithOptions(UUID id);
 }
